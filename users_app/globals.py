391a9b5878318e236d078da2c955de0512c951ab
kabutoblanco <mdquilindo@unicauca.edu.co>
2020-03-03 11:08:14 -0500

    url server front

--- users_app/globals.py
+++ users_app/globals.py
@@ -1,6 +1,6 @@
 #Variables de entorno para configuración de rutas URL de la app
 
-URL_System = "http://locahost:3000/"
+# URL_System = "http://locahost:3000/"
 URL_System = "https://clindesign-78918.web.app/"
-URL_RecoveryPassword = URL_System+"user/recovery/"
-URL_ForgetPassword = URL_System+"user/forget/"
+URL_RecoveryPassword = "{}{}".format(URL_System, "user/recovery/")
+URL_ForgetPassword = "{}{}".format(URL_System, "user/forget/")
